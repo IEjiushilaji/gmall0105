9b2f4a78da9b331e9be96c4505f6f410d7112d35
zhuangb1 <q2063087418@163.com>
2019-10-16 13:24:34 +0800

    调整项目模块： 新增：service-util; web-util; common-util

--- gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -3,9 +3,6 @@ package com.atguigu.gmall.user.mapper;
 import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import tk.mybatis.mapper.common.Mapper;
 
-/**
- *
- */
 public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
 
 }
